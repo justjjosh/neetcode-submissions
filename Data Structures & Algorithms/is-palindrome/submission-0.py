dd1776695ecd2bdfb083f152d71f8b67ec0aa7d4
Raji Olatubosun Joshua <rajiolatubosun@gmail.com>
2026-07-24 22:48:20 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #i want to have a left and right pointer
+        L = 0
+        R = len(s) - 1
+        #check for edge cases- a while loop
+        while R > L:
+            if not s[L].isalnum():
+                L += 1
+            elif not s[R].isalnum():
+                R -= 1
+            else:
+                if s[L].lower() != s[R].lower():
+                    return False
+                else:
+                    L += 1
+                    R -= 1
+        return True
+        
