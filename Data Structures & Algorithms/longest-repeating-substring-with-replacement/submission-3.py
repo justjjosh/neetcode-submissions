08423f0385cfbf40a315082e1bee2b4379d3be4b
Raji Olatubosun Joshua <rajiolatubosun@gmail.com>
2026-08-11 16:35:31 +0100

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        #have a hashmap that stores most frequently occuring
+        count = {} #store the letters and their occurences
+        i = 0
+
+
+        res = 0
+        #initialize a two pointer 
+        i = 0
+        for j in range(len(s)):
+            #get the current count of the current value we are on and increment it in count
+            count[s[j]] = 1 + count.get(s[j], 0)
+            #check for window validity
+            while (j - i + 1) - max(count.values()) > k:
+                count[s[i]] -= 1
+                i += 1
+
+            #update res with current maximum valid window
+            res = max(res, j - i + 1 )
+        return res
+
+        
